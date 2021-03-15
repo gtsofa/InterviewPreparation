fa9a9e5326307fa1e8735dd4353b54e4cb555866
gtsofa <jlsnyule@yahoo.com>
2021-03-15 14:59:46 +0300

    improve Hashable implementation with new Hasher API

--- QuizAppRETests/Presentation/Result/ResultHelper.swift
+++ QuizAppRETests/Presentation/Result/ResultHelper.swift
@@ -8,20 +8,21 @@
 import Foundation
 @testable import QuizEngine
 
-extension Result: Hashable {
-    
+extension Result {
     static func make(answers: [Question: Answer] = [:], score: Int = 0) -> Result<Question, Answer> {
         return Result(answers: answers, score: score)
     }
-    
-    public var hashValue: Int {
-        return 1
-    }
-//    func hash(into hasher: inout Hasher) -> Int {
-//        return 1
-//    }
-    
+}
+
+extension Result: Equatable where Answer: Equatable {
     public static func == (lhs: Result<Question, Answer>, rhs: Result<Question, Answer>) -> Bool {
         return lhs.score == rhs.score
+    }
+}
+
+extension Result: Hashable where Answer: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(answers)
+        hasher.combine(score)
     }
 }
